73c0e6bec786ee18da2f06b19b1d95bff13d429f
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2017-11-01 17:17:39 -0700

    Update CDL.py

--- CDL.py
+++ CDL.py
@@ -4,6 +4,10 @@ import json
 
 app = Flask(__name__)
 @app.route("/index.html")
+def get_state_options(counties):
+  for s in counties:
+    if s["counties"] == stateVar += Markup("<option value=\"" + s + "\">" + s + "</option>"):
+      return stateVar
 
 if __name__ == '__main__':
   main()
