e0c75068db3c259f36618bb3bfb5c22129d2c4e3
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2017-11-03 09:55:27 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,11 +1,11 @@
 from flask import Flask, url_for, request, Markup, render_template, flash, Markup
 import os
 import json
-with open('county_demographics.json') as demographics_data:
-    counties = json.load(demographics_data)
 app = Flask(__name__)
 @app.route('/')
 def main():
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
     return render_template("index.html",options = get_state_options(counties))
 def get_state_options(state):
     options = ""
